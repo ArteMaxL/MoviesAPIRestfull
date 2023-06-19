22a244dd5ca346567a1940631e8e63b32d10d97d
Artemio <Artemio@DESKTOP-92BTGL5>
2023-06-18 23:35:49 -0300

    Added encryption method
    Added the Utils folder and the Encrypt class with a GetMd5 method to encrypt and decrypt the password.

---
+++ MoviesAPI/Utils/Encrypted.cs
@@ -0,0 +1,27 @@
+﻿using XSystem.Security.Cryptography;
+
+namespace MoviesAPI.Utils
+{
+    public static class Encrypted
+    {
+        /// <summary>
+        /// Method to encrypt the password with MD5
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static string GetMd5(string password)
+        {
+            MD5CryptoServiceProvider x = new MD5CryptoServiceProvider();
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(password);
+            data = x.ComputeHash(data);
+            string response = string.Empty;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                response += data[i].ToString("x2").ToLower();
+            }
+
+            return response;
+        }
+    }
+}
