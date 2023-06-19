8ef5011f0e87500f8e030e221abd2794d03fb60e
Artemio <Artemio@DESKTOP-92BTGL5>
2023-06-19 03:52:28 -0300

    Added UsersControllers
    Added methods: GetUser, GetUsers, Register, and, Login.
    Corrected a bug in secretKey (too short key) in appsettings.Development.js.

---
+++ MoviesAPI/Controllers/UsersController.cs
@@ -0,0 +1,122 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using MoviesAPI.Models;
+using MoviesAPI.Models.Dtos;
+using MoviesAPI.Repository;
+using MoviesAPI.Repository.IRepository;
+using System.Net;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        protected ApiResponse _apiResponse;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+            _apiResponse = new();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetUsers()
+        {
+            var listUsers = _userRepository.GetUsers();
+            var listUsersDto = new List<UserDto>();
+
+            foreach (var user in listUsers)
+            {
+                listUsersDto.Add(_mapper.Map<UserDto>(user));
+            }
+
+            return Ok(listUsersDto);
+        }
+
+        [HttpGet("{userId:int}", Name = "GetUser")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetUser(int userId)
+        {
+            var itemUser = _userRepository.GetUser(userId);
+
+            if (itemUser == null)
+            {
+                return NotFound();
+            }
+
+            var itemUserDto = _mapper.Map<UserDto>(itemUser);
+
+            return Ok(itemUserDto);
+        }
+
+        [HttpPost("Register")]
+        [ProducesResponseType(201, Type = typeof(UserDto))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Register([FromBody] UserRegisterDto userRegisterDto)
+        {
+            bool isUserNameUnique = _userRepository.IsUniqueUserName(userRegisterDto.UserName);
+
+            if (!isUserNameUnique)
+            {
+                _apiResponse.StatusCode = HttpStatusCode.BadRequest;
+                _apiResponse.IsSuccess = false;
+                _apiResponse.ErrorMessages.Add("Username already exist.");
+
+                return BadRequest(_apiResponse);
+            }
+
+            var user = await _userRepository.Register(userRegisterDto);
+            if (user == null)
+            {
+                _apiResponse.StatusCode = HttpStatusCode.BadRequest;
+                _apiResponse.IsSuccess = false;
+                _apiResponse.ErrorMessages.Add($"Failed to register {userRegisterDto.UserName}");
+
+                return BadRequest(_apiResponse);
+            }
+
+            _apiResponse.StatusCode = HttpStatusCode.OK;
+            _apiResponse.IsSuccess = true;
+
+            return Ok(_apiResponse);
+        }
+
+        [HttpPost("Login")]
+        [ProducesResponseType(201, Type = typeof(UserDto))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Login([FromBody] UserLoginDto userLoginDto)
+        {
+            var loginResponse = await _userRepository.Login(userLoginDto);
+
+            if (loginResponse.User == null || string.IsNullOrEmpty(loginResponse.Token))
+            {
+                _apiResponse.StatusCode = HttpStatusCode.BadRequest;
+                _apiResponse.IsSuccess = false;
+                _apiResponse.ErrorMessages.Add("The username or password are incorrect.");
+
+                return BadRequest(_apiResponse);
+            }
+
+            _apiResponse.StatusCode = HttpStatusCode.OK;
+            _apiResponse.IsSuccess = true;
+            _apiResponse.Result = loginResponse;
+
+            return Ok(_apiResponse);
+        }
+    }
+}
