22cb556d53533c9c0197c02697e5d7ca0450a01d
Artemio <Artemio@DESKTOP-92BTGL5>
2023-06-18 23:32:57 -0300

    Added User Repository
    Partial implementation of the interface.

---
+++ MoviesAPI/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using MoviesAPI.Models;
+using MoviesAPI.Models.Dtos;
+
+namespace MoviesAPI.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        ICollection<User> GetUsers();
+        User GetUser(int id);
+        bool IsUniqueUserName(string name);
+        Task<UserLoginResponseDto> Login(UserLoginDto userLoginDto);
+        Task<User> Register(UserRegisterDto userRegisterDto);
+    }
+}
---
+++ MoviesAPI/Repository/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using MoviesAPI.Data;
+using MoviesAPI.Models;
+using MoviesAPI.Models.Dtos;
+using MoviesAPI.Repository.IRepository;
+using MoviesAPI.Utils;
+using XSystem.Security.Cryptography;
+
+namespace MoviesAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _bd;
+
+        public UserRepository(ApplicationDbContext bd)
+        {
+            _bd = bd;
+        }
+        public User GetUser(int id)
+        {
+            return _bd.Users.FirstOrDefault(u => u.Id == id);
+        }
+
+        public ICollection<User> GetUsers()
+        {
+            return _bd.Users.OrderBy(u => u.UserName).ToList();
+        }
+
+        public bool IsUniqueUserName(string name)
+        {
+            return _bd.Users.FirstOrDefault(u => u.UserName == name) == null;
+        }       
+
+        public async Task<User> Register(UserRegisterDto userRegisterDto)
+        {
+            var encryptedPassword = Encrypted.GetMd5(userRegisterDto.Password);
+
+            User user = new User 
+            { 
+                UserName = userRegisterDto.UserName, 
+                Password = encryptedPassword,
+                Name = userRegisterDto.Name,
+                Role = userRegisterDto.Role
+            };
+
+            _bd.Users.Add(user);
+            await _bd.SaveChangesAsync();
+            user.Password = encryptedPassword;
+
+            return user;
+        }
+
+        public Task<UserLoginResponseDto> Login(UserLoginDto userLoginDto)
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
